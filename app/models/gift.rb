18f95733a5238f4bae91b58aeb246686e624ddeb
Cameron Cundiff <github@ckundo.com>
2016-02-06 19:19:30 -0500

    Add assets

--- app/models/gift.rb
+++ app/models/gift.rb
@@ -12,7 +12,7 @@ class Gift < ActiveRecord::Base
   def fetch_description
     uri = URI.parse(url)
     Net::HTTP.SOCKSProxy('127.0.0.1', 9050).start(uri.host, uri.port) do |http|
-      res = http.get(uri.path)
+      res = http.get(uri.path).body
       doc = Nokogiri::HTML(res)
       update_attributes(
         description: doc.css("#info > h3")
